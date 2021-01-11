d75c89c55ee0b6f56d12b8250cae89bb7b5fe73e
Sören <34400296+soerenengels@users.noreply.github.com>
2021-01-11 03:38:31 +0100

    update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,36 @@
 <template>
-  <header>
-    <Title>Recherche-Plattform</Title>
-  </header>
   <main>
-    <p v-if="!stories">
-      Hallo {{ user.name }}!<br />Die vorliegende Recherche-Plattform ist
-      <a href="https://github.com/soerenengels/recherche-plattform"
-        >Work in Progress</a
-      >. Sie basiert auf dem Ansatz der Story-based Inquiry Mark Lee Hunter und
-      Luuk Sengers.<br />
-      <router-link to="/hypothese">Starte eine neue Story.</router-link>
-      <!-- <Button type="add" @click="navToNew()" /> -->
-    </p>
+    <LayoutTwoThirds>
+      <template #main>
+        <header>
+          <Title>Recherche-Plattform</Title>
+        </header>
+        <p>
+          Hallo User:in!<br />Die vorliegende Recherche-Plattform ist
+          <a href="https://github.com/soerenengels/recherche-plattform"
+            >Work in Progress</a
+          >. Sie basiert auf dem Ansatz der Story-based Inquiry Mark Lee Hunter
+          und Luuk Sengers.<br />
+          <router-link to="/hypothese">Starte eine neue Story.</router-link>
+        </p>
+      </template>
+      <template #context>
+        <router-link to="/hypothese" class="rarr">&rarr;</router-link>
+      </template>
+    </LayoutTwoThirds>
   </main>
 </template>
 
 <script>
 // @ is an alias to /src
 import Title from "@/components/Title.vue";
-/* import Button from "@/components/Button.vue"; */
+import LayoutTwoThirds from "@/components/LayoutTwoThirds.vue";
 
 export default {
   name: "Home",
-  /* setup() {
-    const navToNew = () => this.$router.push("/new");
-    return {
-      navToNew,
-    };
-  }, */
-  data() {
-    return {
-      user: {
-        name: "User:in",
-      },
-      stories: false,
-    };
-  },
   components: {
     Title,
-    /* Button, */
+    LayoutTwoThirds,
   },
 };
 </script>
@@ -47,5 +39,18 @@ export default {
 a {
   font-size: 1em;
   text-decoration: underline;
+}
+.rarr {
+  font-size: 15rem;
+  content: "&rarr";
+  width: 15rem;
+  height: 15rem;
+  padding: 2.5rem;
+  position: absolute;
+  bottom: 3rem;
+  right: 3rem;
+  background-color: rgb(249, 185, 138);
+  text-decoration: none;
+  border-radius: 10rem;
 }
 </style>
